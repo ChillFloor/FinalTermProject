7429ea1a61435b2183dd4c65e4eed4159376ca8e
ChillFloor <david888@bu.edu>
2025-12-08 19:23:11 -0500

    Changed logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 const Logo = (props) => (
     <svg
-        width="800px"
-        height="800px"
+        width="50px"
+        height="50px"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
